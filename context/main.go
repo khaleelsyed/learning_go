9b1df7676699bcc15b02315e21c75cfca17b435e
khaleelsyed <khaleelsyed@users.noreply.github.com>
2025-01-12 00:09:51 +0000

    attempt using context with cancel

--- context/main.go
+++ context/main.go
@@ -1,3 +1,35 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+)
+
+const answerToEverything = 42
+
+func handleAPICall(timeoutSeconds int) {
+	ctx, cancel := context.WithTimeoutCause(context.Background(), time.Duration(timeoutSeconds)*time.Second, fmt.Errorf("%s", fmt.Sprintf("Over %d seconds have already elapsed", timeoutSeconds)))
+	defer cancel()
+
+	for {
+		go func(ctx context.Context) {
+			value, err := slowAPICall(ctx)
+			if err != nil {
+				log.Fatal(err)
+			} else {
+				fmt.Println(value)
+				return
+			}
+		}(ctx)
+		time.Sleep(time.Millisecond * 500)
+	}
+}
+
+func slowAPICall(ctx context.Context) (int, error) {
+	time.Sleep(time.Second * 1)
+	return answerToEverything, nil
+}
+
+func main() { handleAPICall(2) }
